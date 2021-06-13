b7331af0f2f4279399c8b2d4a03edd2fade713ed
mzaini30 <muhzaini30@gmail.com>
2021-06-13 21:44:12 +0800

    upload

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>WYSIWYG</title>
+</svelte:head>
+
+<div class="w-full p-10 h-screen bg-green-200">
+	<div class="rounded-3xl bg-white border shadow p-3">
+		<div class="focus:outline-none border-red-500 border rounded p-2" contenteditable bind:innerHTML={isi}></div>
+		<textarea name="" id="" cols="30" rows="10" bind:value={isi} class="focus:outline-none border-red-500 border rounded p-2"></textarea>
+	</div>
+</div>
+
+<script>
+	let isi
+</script>
